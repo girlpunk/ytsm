d7a2a951994963aa25a1139aa8e3587cc8aa08d8
Jacob Mansfield [root@Helix] <cyberjacob@gmail.com>
2021-03-14 18:21:05 +0000

    Migrate to Uvicorn

--- docker/init.sh
+++ docker/init.sh
@@ -3,4 +3,5 @@
 ./manage.py migrate --noinput
 ./manage.py collectstatic --noinput
 #daphne -b 0.0.0.0 -p 8000 YtManager.asgi:application
-gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 YtManager.asgi:application
+#gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 YtManager.asgi:application
+uvicorn YtManager.asgi:application --host 0.0.0.0 --port 8000
